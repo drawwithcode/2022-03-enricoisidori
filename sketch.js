c02a3769f954f3b2c5602d354993c87f6cfaaf14
Enrico <enrico.isidori@gmail.com>
2022-11-01 23:37:10 +0100

    Time perceptions

--- sketch.js
+++ sketch.js
@@ -1,16 +1,53 @@
+let capture;
+let song;
+let vol;
+let h;
+let myFont;
+let showText = true;
+
 function preload() {
-	// put preload code here
+  myFont = loadFont("assets/NeueMetana-Bold.otf");
+  song = loadSound("assets/audio.mp3");
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO);
+  capture.hide();
+  background(0);
+}
+function draw() {
+  h = height;
+
+  if (capture.loadedmetadata) {
+    push();
+    translate(width, 0);
+    scale(-1, 1);
+    pop();
+    image(capture, 0, 0, width, height);
+
+    for (let i = 0; i < h; i++) {
+      let p = get(mouseX, i);
+      line(1, i, width, i);
+      stroke(p);
+    }
+  }
+
+  if (showText) {
+    fill("255");
+    textFont(myFont);
+    textSize(36);
+    textAlign(CENTER);
+    let txt = text("CLICK TO START", width / 2, height / 2);
+  }
 }
 
-function draw() {
-	// put drawing code here
+function mousePressed() {
+  song.play();
+  showText = false;
+}
+
+function mouseMoved() {
+  vol = 1 / (mouseY / 200);
+  song.setVolume(vol);
 }
